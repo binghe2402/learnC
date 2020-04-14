d4d07320ec420d6d6450c0014d4d364b46652ed3
binghe2402 <binghe2402@hotmail.com>
2020-04-15 00:08:54 +0800

    两个test

---
+++ Test/test_for_tab_printf.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+    printf("\t1\t2\t-3\n");
+    printf("\t-1\t-2\t3");
+    return 0;
+}
---
+++ Test/test_printf_before_for_segment_fault.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+    printf("123");
+    char *a = "hello";
+    *a = 'a';
+    return 0;
+}
